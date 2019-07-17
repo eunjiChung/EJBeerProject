992d5a6971c82ba4bb55a21941b71f188a760234
eunji <twih1203@gmail.com>
2019-07-17 15:17:58 +0900

    First push

---
+++ BeerProject🍺/BeerProject🍺/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerProject🍺
+//
+//  Created by DEV_MOBILE_IOS_JUNIOR on 17/07/2019.
+//  Copyright © 2019 DEV_MOBILE_IOS_JUNIOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
